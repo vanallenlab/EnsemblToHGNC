0e6eb7e98202e09b2644b0a0c6471d330ad9f064
Eric Kofman <ericrkofman@gmail.com>
2018-04-19 10:55:50 -0400

    Also take ENST

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # EnsemblToHGNC
 
-This script takes in a tab-separated file containing at least one column of Ensembl IDs and a string indicating the
-header for this column, and outputs a tab-separated file identical to the input file except that it has an additional
-column containing mapped HGNC gene symbols for each row.
+This script takes in a tab-separated file containing at least one column of Ensembl gene or transcript IDs (ENSG/ENST)
+and a string indicating the header for this column, and outputs a tab-separated file identical to the input file except
+that it has an additional column containing mapped HGNC gene symbols for each row.
 
-The Ensembl-HGNC symbol mapping is derived from downloads at this site: https://www.genenames.org/cgi-bin/download. Data is based on Ensembl release 91, from December 2017.
+The Ensembl-HGNC symbol mapping is derived from downloads at this site: https://www.genenames.org/cgi-bin/download. Data is based on Ensembl release 92, from April 2018.
+The ENSG-ENST symbol mapping is derived from Ensembl's biomart: http://useast.ensembl.org/biomart/. Data is based on Ensemble release 92, from April 2018.
 
 --------------------------------------------------------------------------------------------------------------------
 # Example usages:
--- ensemblToHGNC/EnsemblToHGNC.py
+++ ensemblToHGNC/EnsemblToHGNC.py
@@ -1,9 +1,11 @@
 import argparse
 import os
 import pandas as pd
+import sys
 
 package_path = os.path.dirname(os.path.realpath(__file__))
-ENSEMBL_TO_HGNC_PATH = os.path.join(package_path, 'HGNC_Ensembl_mapping_01_03_2018.txt')
+ENSG_TO_HGNC_PATH = os.path.join(package_path, 'HGNC_Ensembl_mapping_04_18_2018.txt')
+ENST_TO_ENSG_PATH = os.path.join(package_path,'ENST_ENSG_MAPPING_04_18_2018.txt')
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # This script takes in a tab-separated file containing at least one column of Ensembl IDs and a string indicating the
@@ -18,9 +20,9 @@ ENSEMBL_TO_HGNC_PATH = os.path.join(package_path, 'HGNC_Ensembl_mapping_01_03_20
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
-def load_ensembl_hgnc_mapping(file_path):
-    """Load in the file that contains the mappings between Ensembl IDs and HGNC gene symbols, returning a dictionary
-    mapping of this information."""
+def load_ensg_hgnc_mapping(file_path):
+    """Load in the file that contains the mappings between Ensembl Gene IDs and HGNC gene symbols,
+    returning a dictionary mapping of this information."""
     mapping = pd.read_csv(file_path, sep='\t')
     mapping_dict = {}
 
@@ -31,42 +33,94 @@ def load_ensembl_hgnc_mapping(file_path):
     return mapping_dict
 
 
-def add_symbol(input_df, ensembl_to_hgnc_mapping, ensembl_header):
-    """Take in a dataframe containing the original Ensembl column and add to it a column containing gene symbol"""
-    def to_symbol(ensembl_id):
+def load_enst_ensg_mapping(file_path):
+    mapping = pd.read_csv(file_path, sep='\t')
+    mapping_dict = {}
+
+    def add_row_to_mapping_dict(r):
+        mapping_dict[r['Transcript stable ID']] = r['Gene stable ID']
+    mapping.apply(add_row_to_mapping_dict, axis=1)
+
+    return mapping_dict
+
+def map_enst_to_hgnc(enst_ensg, ensg_hgnc):
+    """Make a dictionary mapping ENST IDs to HGNC gene symbols"""
+    enst_hgnc_mapping = {}
+    for enst, ensg in enst_ensg.items():
+        enst_hgnc_mapping[enst] = ensg_hgnc.get(ensg)
+
+    return enst_hgnc_mapping
+
+
+def add_symbol_from_enst(input_df, enst_to_hgnc_mapping, enst_header):
+    """Take in a dataframe containing the original ENST column and add to it a column containing gene symbol"""
+    def enst_to_symbol(enst):
         try:
-            symbol = ensembl_to_hgnc_mapping[ensembl_id]
+            symbol = enst_to_hgnc_mapping[enst]
         except KeyError:
             try:
                 # Try just mapping to the Ensembl ID section before the '.'
-                symbol = ensembl_to_hgnc_mapping[ensembl_id.split('.')[0]]
+                symbol = enst_to_hgnc_mapping[enst.split('.')[0]]
             except KeyError:
                 return 'Unknown'
         return symbol
 
-    input_df['Gene Symbol'] = input_df.apply(lambda row: to_symbol(row[ensembl_header]), axis=1)
+    input_df['Gene Symbol'] = input_df.apply(lambda row: enst_to_symbol(row[enst_header]), axis=1)
+    return input_df
+
+
+def add_symbol_from_ensg(input_df, ensg_to_hgnc_mapping, ensg_header):
+    """Take in a dataframe containing the original ENSG column and add to it a column containing gene symbol"""
+    def ensg_to_symbol(ensg):
+        try:
+            symbol = ensg_to_hgnc_mapping[ensg]
+        except KeyError:
+            try:
+                # Try just mapping to the Ensembl ID section before the '.'
+                symbol = ensg_to_hgnc_mapping[ensg.split('.')[0]]
+            except KeyError:
+                return 'Unknown'
+        return symbol
+
+    input_df['Gene Symbol'] = input_df.apply(lambda row: ensg_to_symbol(row[ensg_header]), axis=1)
     return input_df
 
 
 def main():
     parser = argparse.ArgumentParser(description='Translate Ensembl IDs to HGNC symbols')
     parser.add_argument('input_file', metavar='input_file', type=str)
-    parser.add_argument('ensembl_header', metavar='ensembl_header', type=str)
+    parser.add_argument('--ensg_header', metavar='ensg_header', type=str)
+    parser.add_argument('--enst_header', metavar='enst_header', type=str)
     parser.add_argument('--output_path', metavar='output_path', type=str)
 
     args = parser.parse_args()
 
     input_file = args.input_file
-    ensembl_header = args.ensembl_header
+    ensg_header = args.ensg_header
+    enst_header = args.enst_header
     output_path = args.output_path
 
-    ensembl_to_hgnc = load_ensembl_hgnc_mapping(ENSEMBL_TO_HGNC_PATH)
+    if enst_header and ensg_header:
+        sys.exit("Only one of enst_header or ensg_header should be provided\n")
+    if not enst_header and not ensg_header:
+        sys.exit("One of enst_header or ensg_header must be provided\n")
+
     input_df = pd.read_csv(input_file, sep='\t')
 
-    # Generate a dataframe with Gene Symbol column added
-    output_df = add_symbol(input_df, ensembl_to_hgnc, ensembl_header)
+    ensg_to_hgnc = load_ensg_hgnc_mapping(ENSG_TO_HGNC_PATH)
+    enst_to_ensg = load_enst_ensg_mapping(ENST_TO_ENSG_PATH)
 
-    output_filename = '{}.{}'.format(input_file.split('/')[-1], 'hgnc_symbols')
+    if ensg_header:
+        # Generate a dataframe with Gene Symbol column added
+        output_df = add_symbol_from_ensg(input_df, ensg_to_hgnc, ensg_header)
+    elif enst_header:
+        enst_to_hgnc = map_enst_to_hgnc(enst_to_ensg, ensg_to_hgnc)
+        output_df = add_symbol_from_enst(input_df, enst_to_hgnc, enst_header)
+    else:
+        # Should never get to this line...
+        output_df = input_df
+
+    output_filename = '{}.{}.{}'.format(os.path.basename(input_file).split('.')[0], 'hgnc_symbols', 'tsv')
 
     # If output path is provided, output location is based on this -- otherwise, it is based on input file location
     if output_path:
